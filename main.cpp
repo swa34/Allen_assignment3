016e58e613f8b4b8f6438936e8b73d0356e76685
swa34 <sa69508@uga,edu>
2021-10-13 18:29:55 -0400

    updateddelete main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include<vector>
 #include<string>
+#include <sstream>
 #include "DoublyLinkedList.h"
 using namespace std;
+
 int main() {
   DoublyLinkedList<int> intList;
   DoublyLinkedList<float> dbList;
@@ -26,6 +28,73 @@ int main() {
 	}
 	//intList.print();
 	fs.close();
+	cout << "insert (i), delete (d), length (l), print (p), deleteSub (b), mode (m),\n"
+			"printReverse(r), swapAtl(s), quit (q)" << endl;
+	bool loop = true;
+	char character; // character input when entering a command
+
+	while (loop) {
+	  cout << "\nEnter a command: ";
+	  cin >> character;
+	  cout << "\n";
+
+	  switch (character) {
+		case 'i': {
+		  int num;
+		  cout << "Number to insert: ";
+		  cin >> num;
+		  intList.insertItem(num);
+		  cout << endl;
+		  intList.print();
+
+		  cout << endl;
+
+		  break;
+		}
+		case 'd': {
+		  int num;
+		  string str;
+		  float flt;
+		  string s;
+		  int x;
+		  cout << "Number to delete: ";
+		  cin.ignore();
+		  getline( cin, str );
+		  istringstream iss(str);
+		  while(iss >> x){
+			intList.deleteItem(x);
+
+		  }
+		  intList.print();
+
+
+		  break;
+		}
+		case 'p': {
+		  intList.print();
+		  stringList.print();
+		  dbList.print();
+		  break;
+		}
+		case 'r': {
+		  intList.printReverse();
+		  intList.print();
+		  stringList.printReverse();
+		  stringList.print();
+		  break;
+		}
+		case 'q': { // quit command that stops the while loop and exits the program
+		  cout << "Quitting program..." << endl;
+		  loop = false;
+		  break;
+		}
+		default: { // this will print when the command is invalid
+		  cout << "Invalid command, try again!" << endl;
+		  break;
+		}
+
+	  }
+	}
   } else if (input == 'f') {
 	ifstream fs;
 	"C:\\Users\\scott\\CLionProjects\\Allen_assignment2\\input.txt";
@@ -55,64 +124,72 @@ int main() {
   } else {
 	cout << "Invalid entry";
   }
-  cout << "insert (i), delete (d), length (l), print (p), deleteSub (b), mode (m),\n"
-		  "printReverse(r), swapAtl(s), quit (q)" << endl;
-  bool loop = true;
-  char character; // character input when entering a command
-
-  while (loop) {
-	cout << "\nEnter a command: ";
-	cin >> character;
-	cout << "\n";
-
-	switch (character) {
-	  case 'i': {
-		int num;
-		cout << "Number to insert: ";
-		cin >> num;
-		intList.insertItem(num);
-		cout << endl;
-		intList.print();
-
-		cout << endl;
-
-		break;
-	  }
-	  case 'd': {
-		int num;
-
-		cout << "Number to delete: ";
-		cin >> num;
-		intList.deleteItem(num);
-		cout << endl;
-		intList.print();
-
-		cout << endl;
-
-		break;
-	  }
-	  case 'p': { // insert command that inserts a node in the linked list
-		intList.print();
-		stringList.print();
-		dbList.print();
-		break;
-	  }
-	  case 'r': { // insert command that inserts a node in the linked list
-		intList.printReverse();
-		intList.print();
-		break;
-	  }
-	  case 'q': { // quit command that stops the while loop and exits the program
-		cout << "Quitting program..." << endl;
-		loop = false;
-		break;
-	  }
-	  default: { // this will print when the command is invalid
-		cout << "Invalid command, try again!" << endl;
-		break;
-	  }
-
-	}
-  }
+//  cout << "insert (i), delete (d), length (l), print (p), deleteSub (b), mode (m),\n"
+//		  "printReverse(r), swapAtl(s), quit (q)" << endl;
+//  bool loop = true;
+//  char character; // character input when entering a command
+//
+//  while (loop) {
+//	cout << "\nEnter a command: ";
+//	cin >> character;
+//	cout << "\n";
+//
+//	switch (character) {
+//	  case 'i': {
+//		int num;
+//		cout << "Number to insert: ";
+//		cin >> num;
+//		intList.insertItem(num);
+//		cout << endl;
+//		intList.print();
+//
+//		cout << endl;
+//
+//		break;
+//	  }
+//	  case 'd': {
+//		int num;
+//		string str;
+//		float flt;
+//		string s;
+//		int x;
+//		cout << "Number to delete: ";
+//		cin.ignore();
+//		getline( cin, str );
+//		  istringstream iss(str);
+//		  while(iss >> x){
+//		  intList.deleteItem(x);
+//
+//		}
+//           intList.print();
+//
+//
+//		break;
+//	  }
+//	  case 'p': {
+//		intList.print();
+//		stringList.print();
+//		dbList.print();
+//		break;
+//	  }
+//	  case 'r': {
+//		intList.printReverse();
+//		intList.print();
+//		stringList.printReverse();
+//		stringList.print();
+//		break;
+//	  }
+//	  case 'q': { // quit command that stops the while loop and exits the program
+//		cout << "Quitting program..." << endl;
+//		loop = false;
+//		break;
+//	  }
+//	  default: { // this will print when the command is invalid
+//		cout << "Invalid command, try again!" << endl;
+//		break;
+//	  }
+//
+//	}
+//  }
 }
 
